374b3d6a603b744dc5d5f2ac129e1a28b91fb2c7
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-19 17:41:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Add your code here
+class Dog
+  attr_accessor :dog
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @dog
+  end
+  
+  
+end
